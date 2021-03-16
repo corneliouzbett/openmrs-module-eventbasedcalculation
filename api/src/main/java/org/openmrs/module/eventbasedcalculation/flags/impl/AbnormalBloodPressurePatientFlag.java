879d6b4f6a6f7239f01a4ace2627e0fa3e721de1
corneliouzbett <kbett68@gmail.com>
2021-03-16 13:50:08 +0300

    Add abnormal blood pressure patient flag

---
+++ api/src/main/java/org/openmrs/module/eventbasedcalculation/flags/impl/AbnormalBloodPressurePatientFlag.java
@@ -0,0 +1,98 @@
+package org.openmrs.module.eventbasedcalculation.flags.impl;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.openmrs.Obs;
+import org.openmrs.Patient;
+import org.openmrs.calculation.patient.PatientCalculation;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.eventbasedcalculation.api.calculations.bloodpressure.DiastolicBloodPressureCalculation;
+import org.openmrs.module.eventbasedcalculation.api.calculations.bloodpressure.SystolicBloodPressureCalculation;
+import org.openmrs.module.eventbasedcalculation.flags.OpenMrsPatientFlag;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * For demonstration purposes. This blood pressure ranges aren't accurate (maybe)
+ * Low blood pressure is less common
+ *
+ * NORMAL - less than 120 systolic(upper #) and less than 80 diastolic(Lower #)
+ * ELEVATED - 120-129 systolic(upper #) and less than 80 diastolic(Lower #)
+ * HYPERTENSION (Stage 1) - 130-139 systolic(upper #) or 80-89 diastolic(Lower #)
+ * HYPERTENSION (Stage 2) - 140 or higher or 90 or higher
+ * HYPERTENSIVE CRISIS - higher than 180 or higher than 120 diastolic(Lower #)
+ */
+
+@Slf4j
+@Component
+@Setter(AccessLevel.MODULE)
+@Getter(AccessLevel.MODULE)
+public class AbnormalBloodPressurePatientFlag extends BasePatientFlag implements OpenMrsPatientFlag<Patient> {
+
+    @Override
+    public void evaluate(Patient patient) {
+        this.evaluate(Collections.singleton(patient));
+    }
+
+    @Override
+    public void evaluate(Collection<Patient> patients) {
+        Set<Integer> patientIds = getPatientIds(patients);
+        PatientCalculation diastolicCalculation = getCalculation(DiastolicBloodPressureCalculation.class);
+        PatientCalculation systolicCalculation = getCalculation(SystolicBloodPressureCalculation.class);
+
+        CalculationResultMap systolicMap = calculate(systolicCalculation, patientIds);
+        CalculationResultMap diastolicMap = calculate(diastolicCalculation, patientIds);
+
+        patientIds.forEach( patientId -> {
+            Obs systolicObs = systolicMap.get(patientId).asType(Obs.class);
+            log.info("Systolic blood pressure {}", systolicObs.getValueNumeric());
+
+            Obs diastolicObs = diastolicMap.get(patientId).asType(Obs.class);
+            log.info("Diastolic blood pressure {}", diastolicObs.getValueNumeric());
+
+            handlingBloodPressure(diastolicObs, systolicObs);
+        });
+
+
+    }
+
+    private void handlingBloodPressure(Obs diastolicObs, Obs systolicObs) {
+        if (systolicObs.getValueNumeric() > 180.00 || diastolicObs.getValueNumeric() > 120.00) {
+            // Hypertensive crisis (call 911)
+
+        } else if (isBetween(systolicObs.getValueNumeric(), 120.00, 129.00)
+        && diastolicObs.getValueNumeric() < 80.0) {
+            // Elevated blood pressure
+        }
+    }
+
+    @Override
+    public void createFlag(Integer patientId) {
+
+    }
+
+    @Override
+    public void createFlag(Collection<Integer> cohort) {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDisplay() {
+        return null;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
--- api/src/main/java/org/openmrs/module/eventbasedcalculation/flags/impl/BasePatientFlag.java
+++ api/src/main/java/org/openmrs/module/eventbasedcalculation/flags/impl/BasePatientFlag.java
@@ -1,19 +1,60 @@
 package org.openmrs.module.eventbasedcalculation.flags.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculation;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.patient.PatientCalculationService;
 import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.eventbasedcalculation.api.calculations.CalculationsProvider;
 
 import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * A marker class
  */
+@Slf4j
 public abstract class BasePatientFlag {
 
     BasePatientFlag() {
+    }
+
+    protected static boolean isBetween(double value, double lowerBound, double upperBound) {
+        return value >= lowerBound && value <= upperBound;
+    }
+
+    /**
+     *  Checks if the value passed in is in the between the range
+     * @param value the value
+     * @param lowerBound lower bound limit
+     * @param upperBound upper bound limit
+     * @param inclusiveOfUpperBound inclusive of the upper bound
+     * @return {@link java.lang.Boolean}
+     */
+    protected static boolean isBetween(double value, double lowerBound, double upperBound, boolean inclusiveOfUpperBound) {
+        return inclusiveOfUpperBound ? value >= lowerBound && value <= upperBound : value >= lowerBound && value < upperBound;
+    }
+
+    protected static Set<Integer> getPatientIds(Collection<Patient> patients) {
+        return patients.stream().map(Patient::getPatientId).collect(Collectors.toSet());
+    }
+
+    protected static PatientCalculation getCalculation(Class<? extends PatientCalculation> clazz) {
+        return (PatientCalculation) new CalculationsProvider().getCalculation(
+                clazz.getSimpleName(), null);
+    }
+
+    /**
+     *
+     * @param calculation Calculation
+     * @param cohort cohort of patient
+     * @return {@link CalculationResultMap}
+     */
+    protected static CalculationResultMap calculate(PatientCalculation calculation, Collection<Integer> cohort) {
+        return Context.getService(PatientCalculationService.class).evaluate(cohort, calculation, null);
     }
 
     /**
